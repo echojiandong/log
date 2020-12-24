7d57922c4d6944f56d5ca3e0f9669ddd19c45bed
冯建东 <85543010@qq.com>
2020-12-24 16:46:24 +0800

    更新

--- src/OperationService/OperationLogService.php
+++ src/OperationService/OperationLogService.php
@@ -11,15 +11,14 @@ class OperationLogService implements OperationLogServiceInterface
 
     public function recordOperation($request, Closure $next)
     {
-        if('GET' != $request->method()){
-            $input = $request->all();
-            $log = new OperationLog();
-            $log->path = $request->path();
-            $log->method = $request->method();
-            $log->ip = $request->ip();
-            $log->input = json_encode($input, JSON_UNESCAPED_UNICODE);
-            $log->save();
-        }
+        echo 1;
+        $input = $request->all();
+        $log = new OperationLog();
+        $log->path = $request->path();
+        $log->method = $request->method();
+        $log->ip = $request->ip();
+        $log->input = json_encode($input, JSON_UNESCAPED_UNICODE);
+        $log->save();
         return $next($request);
     }
 }
